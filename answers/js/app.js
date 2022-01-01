64cff7be77c184211e26621b83767f90391350ab
shahrbanoo5454 <shahrbanoorakhshan@gmail.com>
2022-01-02 03:06:09 +0330

    add home work1-js

---
+++ answers/js/app.js
@@ -0,0 +1,47 @@
+var a = 5;
+var b = 10;
+var c = "5";
+var d = false;
+
+ alert("welcom to my site")
+ var num=prompt("pleas enter number(1/7)");
+
+if(num==1){
+    console.log((!(b<5) && b >= (a*2)));
+}
+else if(num==2){
+    console.log((!(a == c) || a !== c));
+}
+else if(num==3){
+    console.log(d == false && b-7 > a && d);
+}
+else if(num==4){
+    console.log(a && b && c);
+}
+ else if(num==5){
+     console.log(!a || !d == true || !d);
+ }
+ else if (num==6){
+     console.log(a >= (b-5) && 0);
+ }
+ else if(num==7){
+     console.log(" ")
+      //  (arr || false);
+ }
+ else{
+     console.log("not definde:(");
+   
+
+ }
+//  answers:
+// 1)true
+// 2)true
+// 3)false
+// 4)5
+// 6)0
+// 7)not definne
+
+
+
+
+
