f3a8477c2e2d325efaea8857ac48a8d0778ad611
shahrbanoo5454 <87280526+shahrbanoo5454@users.noreply.github.com>
2022-01-10 00:23:18 +0330

    add h3

---
+++ web3-h3/main.js
@@ -0,0 +1,66 @@
+var clock =parseInt(prompt("what time is it"));
+
+switch(clock) {
+    case 8:
+        document.getElementById('output').innerHTML = '<h1 class="  text-success p-3 bg-dark ">Wake up.</h1>';
+        break;
+        
+    case 9:
+        document.getElementById('output').innerHTML = '<h1 class="  text-success p-3 bg-dark ">Wake up.</h1>';
+        break;
+
+    case 10:
+        document.getElementById('output').innerHTML = '<h1 class="  text-white p-3 bg-dark ">Go to the work.</h1>';
+        break;
+
+    case 11:
+        document.getElementById('output').innerHTML = '<h1 class="  text-white p-3 bg-dark ">Go to the work.</h1>';
+        break;
+
+    case 12:
+        document.getElementById('output').innerHTML = '<h1 class="  text-warning p-3 bg-dark ">Lunch</h1>';
+        break;
+
+    case 13:
+        document.getElementById('output').innerHTML = '<h1 class="  text-white  p-3 bg-dark">Go to work</h1>';
+        break;
+
+    case 14:
+        document.getElementById('output').innerHTML = '<h1 class="  text-white  p-3 bg-dark">Go to work</h1>';
+        break;
+
+    case 15:
+        document.getElementById('output').innerHTML = '<h1 class="text-center text-white p-3 bg-dark ">Go to work</h1>';
+        break;
+
+    case 16:
+        document.getElementById('output').innerHTML = '<h1 class="text-center text-white p-3 bg-dark ">Go to work.</h1>';
+        break;
+
+    case 17:
+        document.getElementById('output').innerHTML = '<h1 class="text-center text-primary p-3 bg-dark ">Gym.</h1>';
+        break;
+
+    case 18:
+        document.getElementById('output').innerHTML = '<h1 class="text-center text-primary bg-dark p-3">Gym.</h1>';
+        break;
+
+    case 19:
+        document.getElementById('output').innerHTML = '<h1 class="text-center text-warning  bg-dark p-3">Dinner time.</h1>';
+        break;
+
+    case 20:
+        document.getElementById('output').innerHTML = '<h1 class="text-center text-info p-3 bg-dark ">Watch TV.</h1>';
+        break;
+
+    case 21:
+        document.getElementById('output').innerHTML = '<h1 class="text-center text-info p-3 bg-dark ">Watch TV.</h1>';
+        break;
+
+    case 22:
+        document.getElementById('output').innerHTML = '<h1 class="text-center text-white-50 p-3 bg-dark ">Time to go to sleep</h1>';
+        break;
+
+    default:
+        document.getElementById('output').innerHTML = '<h1 class="text-center text-danger p-3 bg-dark">You should be sleeping.</h1>';
+}
