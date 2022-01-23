ba812cdd4aaf429c32d129ab189d1442cc5f5840
shahrbanoo5454 <87280526+shahrbanoo5454@users.noreply.github.com>
2022-01-23 14:36:27 +0330

    aded h05

--- 5/app.js
+++ 5/app.js
@@ -1,4 +1,11 @@
-var $num1 = document.getElementById('num1');
-var $num2 = document.getElementById('num2');
-var $answer = document.getElementById('answer');
+var $num1 = document.getElementById('num1').value;
+var $num2 = document.getElementById('num2').value;
+// var $answer = document.getElementById('answer');
 
+function addition(){
+   var answer = $num1+$num2;
+
+//    answer= document.getElementById('answer').value;
+document.getElementById('answer').value = answer;
+
+}
