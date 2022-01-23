b18cd3d9aecc0a61562f33424e108bbc73ff7f90
shahrbanoo5454 <87280526+shahrbanoo5454@users.noreply.github.com>
2022-01-24 02:54:15 +0330

    added homework 05

--- 5/app.js
+++ 5/app.js
@@ -1,11 +1,41 @@
-var $num1 = document.getElementById('num1').value;
-var $num2 = document.getElementById('num2').value;
-// var $answer = document.getElementById('answer');
-
 function addition(){
-   var answer = $num1+$num2;
-
-//    answer= document.getElementById('answer').value;
-document.getElementById('answer').value = answer;
-
+var $num1 = (document.getElementById('num1').value);
+$num1=Number($num1);
+var $num2 = (document.getElementById('num2').value);
+$num2=Number($num2);
+    var sum=($num1+$num2);
+    console.log(sum);
+   document.getElementById('answer').innerHTML='answer is: '+sum;
 }
+
+function subtraction(){
+    var $num1 = (document.getElementById('num1').value);
+    $num1=Number($num1);
+    var $num2 = (document.getElementById('num2').value);
+    $num2=Number($num2);
+        var sum=($num1-$num2);
+        console.log(sum);
+       document.getElementById('answer').innerHTML='answer is: '+sum;
+    }
+
+function multiplication(){
+    var $num1 = (document.getElementById('num1').value);
+     $num1=Number($num1);
+     var $num2 = (document.getElementById('num2').value);
+      $num2=Number($num2);
+          var sum=($num1*$num2);
+          console.log(sum);
+         document.getElementById('answer').innerHTML='answer is: '+sum;
+      }   
+ 
+function division(){
+    var $num1 = (document.getElementById('num1').value);
+     $num1=Number($num1);
+     var $num2 = (document.getElementById('num2').value);
+      $num2=Number($num2);
+          var sum=($num1/$num2);
+          console.log(sum);
+         document.getElementById('answer').innerHTML='answer is: '+sum;
+      }   
+ 
+
